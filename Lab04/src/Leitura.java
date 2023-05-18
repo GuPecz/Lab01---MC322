63a29332ce53aae3b632d724737c383649fa57d4
Gustavo Montenari Pechta <mrpecz@proton.me>
2023-05-18 17:17:25 -0300

    Implementar classe estática para organizar a leitura de dados

---
+++ Lab04/src/Leitura.java
@@ -0,0 +1,81 @@
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class Leitura 
+{
+    private static Scanner input = new Scanner(System.in);
+    
+    public static LocalDate leData()
+    {
+        String data;
+		int dia = 0, mes = 0, ano = 0;
+		boolean dataValida;
+	
+		do
+		{
+			data = input.nextLine();
+			data = data.replaceAll("\\D", "");
+	
+			if (data.length() != 8)
+			{
+				System.out.println("ERRO: Data inválida");
+				dataValida = false;
+			}
+			else
+			{
+				dia = Integer.parseInt(data.substring(0, 2));
+				mes = Integer.parseInt(data.substring(2, 4));
+				ano = Integer.parseInt(data.substring(4, 8));
+	
+				dataValida = Validacao.validarData(dia, mes, ano);
+			}
+		} while (!dataValida);
+	
+		return LocalDate.of(ano, mes, dia);
+    }
+
+    public static String lePalavra()
+	{
+		String nome;
+		boolean nomeValido;
+
+		do
+		{
+			nome = input.nextLine();
+
+			nomeValido = Validacao.validarNome(nome);
+			if (!nomeValido)
+				System.out.println("ERRO: Palavra inválida");
+		} while (!nomeValido);
+
+		return nome;
+	}
+
+    public static int leInt()
+    {
+        String inteiro;
+        boolean intValido = false;
+
+        do
+        {
+            inteiro = input.nextLine();
+
+            try
+            {
+                Integer.parseInt(inteiro);
+                intValido = true;
+            } 
+            catch (NumberFormatException e)
+            {
+                System.out.println("ERRO: Número inválido");
+            }
+        } while (!intValido);
+
+        return Integer.parseInt(inteiro);
+    }
+
+    public static String leString()
+    {
+        return input.nextLine();
+    }
+}
