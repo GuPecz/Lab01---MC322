acbe6d661f2450d99f53ec2b4ca86c72c7ea1b40
Gustavo Montenari Pechta <mrpecz@proton.me>
2023-05-18 17:19:08 -0300

    Atualizar estilo da sintaxe

--- Lab04/src/Validacao.java
+++ Lab04/src/Validacao.java
@@ -9,19 +9,13 @@ public class Validacao
         int resto;
 
         for (int i = ini; i < fim; i++, multiplicador--)
-        {
             verificador += cpf[i] * multiplicador;
-        }
 
         resto = verificador % 11;
         if (resto == 1 || resto == 0)
-        {
             return 0;
-        }
         else
-        {
             return (11 - resto);
-        }
     }
 
     public static int digitoVerificadorCNPJ(int[] cnpj, int ini, int fim)
@@ -32,28 +26,18 @@ public class Validacao
         int j;
 
         if (fim == 12) 
-        {
             j = 1;
-        }
         else
-        {
             j = 0;
-        }
 
         for (int i = ini; i < fim; i++, j++)
-        {
             verificador += cnpj[i] * mults[j];
-        }
 
         resto = verificador % 11;
         if (resto < 2)
-        {
             return 0;
-        }
         else
-        {
             return (11 - resto);
-        }
     }
 
     public static boolean validarCPF(String cpf)
@@ -67,40 +51,28 @@ public class Validacao
         String cpfNumeros = cpf.replaceAll("\\D", "");
 
         if (cpfNumeros.length() != 11)
-        {            
             return false;
-        }
 
         for (int i = 0; i < 11; i++)
-        {
             cpfInt[i] = cpfNumeros.charAt(i) - '0';
-        }
 
         for (int i = 1; i < 11; i++)
-        {
             if (cpfInt[i] != cpfInt[i - 1])
             {
                 todosIguais = false;
                 break;
             }
-        }
 
         if (todosIguais == true)
-        {
             return false;
-        }
 
         verificadorUm = digitoVerificadorCPF(cpfInt, 0, 9);
         verificadorDois = digitoVerificadorCPF(cpfInt, 1, 10);
 
         if (cpfInt[9] == verificadorUm && cpfInt[10] == verificadorDois)
-        {
             return true;
-        }
         else
-        {
             return false;
-        }
     }
 
 
@@ -115,49 +87,36 @@ public class Validacao
         String cnpjNumeros = cnpj.replaceAll("\\D", "");
 
         if (cnpjNumeros.length() != 14)
-        {            
             return false;
-        }
 
         for (int i = 0; i < 14; i++)
-        {
             cnpjInt[i] = cnpjNumeros.charAt(i) - '0';
-        }
+
 
         for (int i = 1; i < 14; i++)
-        {
             if (cnpjInt[i] != cnpjInt[i - 1])
             {
                 todosIguais = false;
                 break;
             }
-        }
 
         if (todosIguais == true)
-        {
             return false;
-        }
 
         verificadorUm = digitoVerificadorCNPJ(cnpjInt, 0, 12);
         verificadorDois = digitoVerificadorCNPJ(cnpjInt, 0, 13);
 
         if (cnpjInt[12] == verificadorUm && cnpjInt[13] == verificadorDois)
-        {
             return true;
-        }
         else
-        {
             return false;
-        }
     }
 
     public static boolean validarNome(String nome)
     {
         for (char c: nome.toCharArray())
-        {
             if (!Character.isLetter(c) && c != ' ')
                     return false;
-        }
 
         return true;
     }
