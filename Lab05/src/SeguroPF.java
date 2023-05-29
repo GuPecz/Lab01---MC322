45b66c2889f14e46486204a684f8d701e209b7cc
Gustavo Montenari Pechta <ra247145@soares.lab.ic.unicamp.br>
2023-05-29 13:56:17 -0300

    Adicionar rascunho de implementação da classe

--- Lab05/src/SeguroPF.java
+++ Lab05/src/SeguroPF.java
@@ -1,4 +1,57 @@
-public class SeguroPF 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class SeguroPF extends Seguro
 {
-    
+    private Veiculo veiculo;
+    private ClientePF cliente;
+
+    public SeguroPF(int id, LocalDate dataInicio, LocalDate dataFim, Seguradora seguradora, ArrayList<Sinistro> listaSinistros, ArrayList<Condutor> listaCondutores, int valorMensal, Veiculo veiculo, ClientePF cliente) 
+    {
+        super(id, dataInicio, dataFim, seguradora, listaSinistros, listaCondutores, valorMensal);
+        this.veiculo = veiculo;
+        this.cliente = cliente;
+    }
+
+    public Veiculo getVeiculo() 
+    {
+        return veiculo;
+    }
+
+    public void setVeiculo(Veiculo veiculo) 
+    {
+        this.veiculo = veiculo;
+    }
+
+    public ClientePF getCliente() 
+    {
+        return cliente;
+    }
+
+    public void setCliente(ClientePF cliente) 
+    {
+        this.cliente = cliente;
+    }
+
+    public void desautorizarCondutor()
+    {
+
+    }
+
+    public void autorizarCondutor()
+    {
+
+    }
+
+    public double calcularValor()
+    {
+        // Stub
+        return 0.0;
+    }
+
+    public boolean gerarSinistro(Sinistro sinistro)
+    {
+        // Stub
+        return false;
+    }
 }
--- Lab05/src/SeguroPJ.java
+++ Lab05/src/SeguroPJ.java
@@ -1,4 +1,57 @@
-public class SeguroPJ
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class SeguroPJ extends Seguro
 {
-    
+    private Frota frota;
+    private ClientePJ cliente;
+
+    public SeguroPJ(int id, LocalDate dataInicio, LocalDate dataFim, Seguradora seguradora, ArrayList<Sinistro> listaSinistros, ArrayList<Condutor> listaCondutores, int valorMensal, Frota frota, ClientePJ cliente) 
+    {
+        super(id, dataInicio, dataFim, seguradora, listaSinistros, listaCondutores, valorMensal);
+        this.frota = frota;
+        this.cliente = cliente;
+    }
+
+    public Frota getFrota() 
+    {
+        return frota;
+    }
+
+    public void setFrota(Frota frota) 
+    {
+        this.frota = frota;
+    }
+
+    public ClientePJ getCliente() 
+    {
+        return cliente;
+    }
+
+    public void setCliente(ClientePJ cliente) 
+    {
+        this.cliente = cliente;
+    }
+
+    public void desautorizarCondutor()
+    {
+
+    }
+
+    public void autorizarCondutor()
+    {
+
+    }
+
+    public double calcularValor()
+    {
+        // Stub
+        return 0.0;
+    }
+
+    public boolean gerarSinistro(Sinistro sinistro)
+    {
+        // Stub
+        return false;
+    }
 }
