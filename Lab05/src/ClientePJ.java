b30ffad7b90dfd40bee21ea35bf8c13a5b4ba373
Gustavo Montenari Pechta <mrpecz@proton.me>
2023-06-06 10:19:33 -0300

    Atualizar implementação de listarVeiculosPorFrota

--- Lab05/src/ClientePJ.java
+++ Lab05/src/ClientePJ.java
@@ -117,7 +117,14 @@ public class ClientePJ extends Cliente
         }
         
         for (Frota frota: listaFrotas)
-        System.out.println(frota);
+        {
+            ArrayList<Veiculo> listaVeiculos = frota.getListaVeiculos();
+            int tam = frota.getListaVeiculos().size();
+
+            System.out.println("Veículos da frota " + frota.getCodigo());
+            for (int i = 0; i < tam - 1; i++)
+                System.out.print("\n" + (i + 1) + listaVeiculos.get(i).getPlaca());
+        }
         
         return true;
     }
